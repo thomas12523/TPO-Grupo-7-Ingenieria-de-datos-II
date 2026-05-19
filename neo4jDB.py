6ca64f43c83a4613e9c0bdb35218d4db8b19c198
Thomas Pacheco <thomasextrem0408@gmail.com>
2026-05-19 00:09:04 -0300

    anadi los 2 RFs que pedia el enunciado para Neo4j

--- neo4jDB.py
+++ neo4jDB.py
@@ -8,16 +8,17 @@ driver = GraphDatabase.driver(
 
 # funciones para crear los nodos de relaciones y atender a los CUs
 
-def vincular_piloto_temporada(nombrePiloto,anioTemporada,podios):
-
+def vincular_piloto_temporada(nombrePiloto,anioTemporada):
+    # IMPORTANTE, en resultado obtenido cuando se registra en cassandra, si entro en el podio hacemos un incremento +1
     with driver.session() as session:
         query="""
         MERGE (p:Piloto {nombre: $nombre})
         MERGE (t:Temporada {anio: $anio})
         MERGE (p) -[r:PARTICIPO_EN]->(t)
-        SET r.podios= $podios
+        ON CREATE SET r.podios= 1
+        ON MATCH SET r.podios= r.podios+1
         """
-        session.run(query,nombre=nombrePiloto,anio=anioTemporada,podios=podios)
+        session.run(query,nombre=nombrePiloto,anio=anioTemporada)
         print("Relacion Creada")
 
 def vincular_carrera_circuito_pais(nombreCarrera,nombreCircuito,nombrePais):
@@ -38,8 +39,40 @@ def vincular_carrera_circuito_pais(nombreCarrera,nombreCircuito,nombrePais):
 
 # ¿Que pilotos han tenido más de 10 podios Y han corrido en más de 5 temporadas?
 
+def pilotosEficientes():
+    print("Los pilotos son:\n")
+    with driver.session() as session:
+        query="""
+        MATCH (p:Piloto) -[r:PARTICIPO_EN]->(t:Temporada)
+        WITH p,sum(r.podios) as totalPodios, count(t) as temporadas
+        WHERE totalPodios>10 AND temporadas>5
+        RETURN p.nombre as "Nombre Piloto",sum(r.podios) as "Podios Totales",count(t) as "Temporadas"
+        """
+    session.run(query)
+    
 
+# ¿Qué paises han tenido mayor cantidad de carreras O que pais tiene mas de 1 circuito?
 
-# ¿Qué pais han tenido mayor cantidad de carreras O que pais tiene mas de 1 circuito?
+def paisConMasCarreras():
+    print("Los Paises en Raking son:\n")
+    with driver.session() as session:
+        query="""
+        MATCH (ca:Carrera)-[:REALIZADO_EN]->(c:Circuito)-[:UBICADO_EN]->(p:Pais)
+        WITH p,sum(ca) as CantidadCarreras
+        RETURN p.nombre,CantidadCarreras
+        ORDER BY CantidadCarreras DESC
+        """
+    session.run(query)
+
+def paisConMas1Circuito():
+    print("Los Paises son:\n")
+    with driver.session() as session:
+        query="""
+        MATCH (c:Circuito)-[r:UBICADO_EN]->(p:Pais)
+        WITH p,count(c) as CantidadCircuitos
+        WHERE CantidadCircuitos>1
+        RETURN p.nombre,CantidadCircuitos
+        """
+    session.run(query)
 
 driver.close()
